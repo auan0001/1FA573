8e220ee079c3358fc9a4c3986926a885a094dd74
auan <august.forsman@outlook.com>
2023-02-15 14:12:03 +0100

    Remade dir structure

--- HE2/Home_Exercise_2.m
+++ HE2/Home_Exercise_2.m
@@ -1,28 +1,45 @@
 % Code for home exercise number 2. 
 % problem focuses on implementation of numeric integration for solution of
 % Green's equation 
+set(0, 'defaulttextinterpreter', 'latex')
+hAxes.TickLabelInterpreter = 'latex';
 
 % Defining parameter values 
 r_max = 30; 
 a = 4; % given constant 
 N = 1000; % number of steps 
-r_values = linspace(0, r_max/3, 1000); 
+r_values = linspace(0, r_max, 1000); 
 y_numeric = zeros(1, length(r_values)); % prealocation
 
 % numeric solutions. This is not efficient, but does not need to be. 
 for i = 1:length(r_values)
-    
     y_numeric(i) = numeric_phi(r_values(i), a, r_max); 
-    
 end 
 
 % plotting the solution 
-hold on
 
+gap = N/50;
+
+abs_error = abs(analytic(a,r_values) - y_numeric);
+
+figure;
+plot(r_values, abs_error)
+grid on
+hold off
+title('Absolute error')
+xlabel('$r$')
+ylabel('$|\phi-\phi_n|$')
+
+figure;
 plot(r_values, analytic(a,r_values))  
-plot(r_values, y_numeric)
-legend('analytic solution','numeric solution')
-
+hold on
+grid on
+plot(r_values(1:gap:end), y_numeric(1:gap:end), 'rs')
+leg = legend({'Analytical $\phi(r)$', 'Numerical $\phi_n(r)$'}, 'location', 'northeast');
+set(leg, 'Interpreter','latex')
+title('Numerical solution')
+xlabel('$r$')
+ylabel('$\phi$')
 hold off
 
 %% DEFINITION OF LOCAL FUNCTIONS 
@@ -31,11 +48,11 @@ hold off
 
 function y = numeric_phi(r, a, r_max)
 
-int_steps  = 1000; % number of integration steps 
+N  = 1000; % number of integration steps 
 
 % we split the sltn in two for readability 
-first = phi_great(a, r)*bode(@less_integ, 0, r, int_steps, a);  
-second = phi_less(a, r)*bode(@great_integ, r, r_max, int_steps, a); 
+first = phi_great(a, r)*bode(@less_integ, 0, r, N, a);  
+second = phi_less(a, r)*bode(@great_integ, r, r_max, N, a); 
 
 y = first + second;  
 
@@ -57,7 +74,7 @@ h = (b-a)/N; % step-length
 
 integral = 0; % prealocation 
 
-for n = 1:4:N-3 % CHECK THIS
+for n = 1:4:N-3
   
     part_1 = 7*f(a_val, a+(n-1)*h) + 32*f(a_val, a+(h*n)); 
     part_2 = 12*f(a_val, a+h*(n+1)) + 32*f(a_val, a+h*(n+2)) + 7*f(a_val, a+h*(n+3)); 
