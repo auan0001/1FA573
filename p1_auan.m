e15bd86b45547eccfe887af987d421d7b04b05a1
auan <august.forsman@outlook.com>
2023-03-03 13:35:45 +0100

    Plots done. Might fix functions and global vars

--- p1_auan.m
+++ p1_auan.m
@@ -5,7 +5,7 @@ set(0, 'defaulttextinterpreter', 'latex')
 hAxes.TickLabelInterpreter = 'latex';
 
 % Spatial domain
-N = 100;
+N = 50;
 rmax = 10;
 bmin = 1e-1;
 rmax_numerical = rmax-1e-6;
@@ -34,24 +34,67 @@ for i = 1:N
   I2(i) = rectangle_rule(FNI2,rmin, sqrt(rmax - rmin),impact(i),N);
 end
 
+theta_n1 = 2*impact.*(I1-I2);
+theta1 = real(FN(impact));
+err1 = abs(theta_n1-theta1);
+
+% Constant potential and energy
+V = 1;
+E = -3*V;
+
+% Integrands
+FNI1 = @(r,b) 1/r^2 / sqrt(1-(b/r)^2);
+FNI2 = @(r,b) 1/r^2 / sqrt(1-(b/r)^2-(V/E));
+
+% Square root term in the denominator of FNI2
+denominator = @(r,b) 1-(b/r).^2-(V/E);
+
+% Analytical solution
+FN = @(b) 2*asin(b/(rmax*sqrt(1-V/E))) - 2*asin(b/rmax);
+
+% Integrate for each rmin
+for i = 1:N
+  rmin = search(rmax, 1e-12, denominator, 0.2, impact(i));
+  I1(i) = rectangle_rule(FNI1,impact(i),sqrt(rmax - impact(i)),impact(i),N);
+  I2(i) = rectangle_rule(FNI2,rmin, sqrt(rmax - rmin),impact(i),N);
+end
 % Numerical solution
-theta_n = 2*impact.*(I1-I2);
-theta = real(FN(impact));
+theta_n2 = 2*impact.*(I1-I2);
+theta2 = real(FN(impact));
+err2 = abs(theta_n2-theta2);
 
 % Plots
-subplot(1,2,1)
-plot(impact, theta)
+subplot(2,2,1)
+plot(impact, theta1)
 grid on
 hold on
-plot(impact,theta_n,'.')
+plot(impact,theta_n1,'.')
 hold off
-title('Scattering angle')
+title('Scattering angle for $E>V_0$')
 xlabel('$b$')
 ylabel('$\theta$')
 legend('Analytical', 'Numerical', 'Location', 'Best')
-subplot(1,2,2)
-semilogy(impact,abs(theta_n-theta),'*-')
-title('Absolute error')
+subplot(2,2,2)
+semilogy(impact,err1,'*-')
+title('Absolute error for $E>V_0$')
+xlabel('$b$')
+ylabel('$L^1(\theta)$')
+hold off
+grid on
+
+subplot(2,2,3)
+plot(impact, theta2)
+grid on
+hold on
+plot(impact,theta_n2,'.')
+hold off
+title('Scattering angle for $E<V_0$')
+xlabel('$b$')
+ylabel('$\theta$')
+legend('Analytical', 'Numerical', 'Location', 'Best')
+subplot(2,2,4)
+semilogy(impact,err2,'*-')
+title('Absolute error for $E<V_0$')
 xlabel('$b$')
 ylabel('$L^1(\theta)$')
 hold off
