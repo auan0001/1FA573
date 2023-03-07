435650b48c329f3a80da060756eb2e4d65b25aab
auan <august.forsman@outlook.com>
2023-03-07 09:19:16 +0100

    Vectorizing in progress. Found a neat function called circshift

--- p3_auan.m
+++ p3_auan.m
@@ -1,27 +1,63 @@
 clear
 
-N = 8;
-print = 1;
+N = 32;
 
-for i = 1:N
-  for j = 1:N
-    A(i,j) = (j-1)+(i-1)*(N)+1;
+% Init spin matrix
+S = lattice_init(N);
+
+% Temperature and spin interaction
+J = 1;
+
+% Length of thermalisation phase
+therm_phase = 300;
+sweep_phase = 300;
+
+% kBT/J
+kBT_J = linspace(0,3,50)./J; 
+
+
+for i = 1:length(kBT_J)
+  for j = 1:therm_phase
+    S = metropolis_hastings(S,kBT_J(i));
+  end
+
+  for j = 1:sweep_phase
+    S = metropolis(S,kBT_J(i));
+    nbrs = lattice_nbrs(S);
+    E(i) = lattice_energy(S, nbrs);
   end
 end
 
-% Spin matrix
-A = (-1).^randi(2,N,N);
+plot(kBT_J,E,'*')
 
-heatmap(A);
-
-for i = 1:N
-  for j = 1:N
-    E(i,j) = 2*A(i,j) ...
-      *(A(i, mod(j-2, N)+1) ...
-      + A(mod(i-2, N)+1, j) ...
-      + A(i ,mod(j, N)+1) ...
-      + A(mod(i, N)+1, j));
+function S = metropolis(S, nbrs, kBT_J)
+  % Assumes square spin lattice
+  N = size(S,1);
+  % Random lattice site
+  site = randi(N*N);
+  % Compute all nbrs once every iter
+  % Compute dE and check spin flip
+  dE = 2*S(site)*nbrs(site); 
+  spin_flip_prob = exp(-dE/kBT_J);
+  if dE <= 0 || rand() <= spin_flip_prob
+    S(site) = -S(site); 
   end
 end
 
+function spin = lattice_init(N)
+  % Assumes square spin lattice
+  spin = (-1).^randi(2,N,N);
+end
 
+function E = lattice_E(S, nbrs)
+  % Energy mean of whole matrix
+  E = mean(S.*nbrs,'all'));
+
+function nbrs = lattice_nbrs(S)
+  % Circular shift to compute neighbours
+  % Similar to MPI_Cart_shift
+  nbrs = circshift(S,[0 1]) ...
+    + circshift(S, [0 -1]) ...
+    + circshift (S,[1 0]) ...
+    + circshift(S, [-1 0]);
+end
