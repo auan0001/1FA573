af43e18e64e22511e12cc8b804fed882f25678b7
auan <august.forsman@outlook.com>
2023-03-08 14:40:37 +0100

    Only computes site neighbours since we are not interested in the energy for now

--- p3_auan.m
+++ p3_auan.m
@@ -1,93 +1,66 @@
 clear
 % Lattice and sample size
-N = 8;
-N_samples = 1000;
+N = [8 16 32];
+N_samples = 200;
 
 % Init spin matrix
-S = lattice_init(N);
 
 % Length of phases
-therm_phase = N_samples;
-sweep_phase = N_samples;
+therm_phase = 1000;
 
 % kBT/J is our unit (from hints)
 J = 1;
-unit_min = 1; 
+unit_min = 0.1; 
 unit_max = 5; 
-kBT_J = linspace(unit_min,unit_max,N_samples)./J; 
 
-% TODO: ising function as driver function
-for i = 1:length(kBT_J)
-  % Thermalization phase for each k_B*T/J
-  for j = 1:therm_phase
-    % nbrs = lattice_nbrs(S);
-    S = metropolis(S, lattice_nbrs(S), kBT_J(i));
+% From high to low temp gives efficient therm phase
+kBT_J = linspace(unit_max,unit_min,100)./J; 
+
+for sz = 1:length(N)
+  S = lattice_init(N(sz));
+  M = zeros(1,length(kBT_J));
+  tic
+  % TODO: ising function as driver function
+  for i = 1:length(kBT_J)
+    M_MC = 0;
+    % Thermalization phase for each k_B*T/J
+    for j = 1:therm_phase
+      S = metropolis(S, kBT_J(i));
+    end
+
+    % Sweep phase for each k_B*T/J
+    for j = 1:N_samples
+      for k = 1:N(sz)*N_samples
+        S = metropolis(S, kBT_J(i));
+        % Accumulate magnetization
+        M_MC = M_MC + lattice_M(S);
+      end
+       % Compute average after every MC step
+      M(i) = M_MC/(N(sz)*N_samples^2);
+    end
   end
+  toc
 
-  % Sweep phase for each k_B*T/J
-  for j = 1:sweep_phase
-    nbrs = lattice_nbrs(S);
-    S = metropolis(S, nbrs, kBT_J(i));
-    % E(i) = lattice_E(S, nbrs);
-    M(i) = lattice_M(S);
-  end
-end
-
-win_min = 50;
-win_step = 1;
-win_max = 200;
-win_tol = 1e-2;
-
-% Interested in magnitude of M this time
-% Probably need to be normalized in some way
-M = abs(M);
-
-% TODO: Vectorize and test
-% Find where the absolute difference of the sums flattens out
-% To obtain a good rolling mean
-% Small window size <-> cheap, unsmooth, unrealistic
-% Too large window size <-> expensive, smooth, unrealistic
-% Optimal window size <-> cheap, smooth, realistic
-win_sz = win_min:win_step:win_max;
-for sz = 1:length(win_sz)
-  M_smooth = movmean(M, win_sz(sz));
-  sum_abs_diff(sz) = sum(abs(M_smooth - M));
-end
-
-% Find 'optimal' i.e cheap window size
-win_opt = find(diff(sum_abs_diff) <= win_tol, 1, 'last');
-% If we cannot find a good size, use the cheapest
-if isempty(win_opt)
-  % Use smallest size
-  win_opt = min(win_sz);
-  disp(['WARNING! Did not find optimal window size'])
-  disp(['Using fallback size: ' num2str(win_opt)])
-else
-  disp(['Window size: ' num2str(win_sz(win_opt))])
-end
-
-% Moving mean of M
-M_mov_mean = movmean(M, win_opt);
-
-figure;
-% plot(kBT_J,M,'.')
+figure(1);
+plot(kBT_J,M,'.')
 hold on
 grid on
-title(['Lattice size N =' num2str(N)])
-plot(kBT_J,M_mov_mean,'-', 'Linewidth',2)
+title(['Lattice sizes N =' num2str(N)])
+% plot(kBT_J,M_mov_mean,'-', 'Linewidth',2)
 xlabel('{k_bT}/J')
 % ylabel('<E>')
 ylabel('M [magnitude]')
+end
+legend('N=8','N=16','N=32')
 hold off
 
-function S = metropolis(S, nbrs, kBT_J)
+function S = metropolis(S, kBT_J)
   % Assumes square spin lattice
   N = size(S,1);
   % Random lattice site
   site = randi(N*N);
-  % Compute all nbrs once every iter
-  % Compute dE and check spin flip
-  dE = 2*S(site)*nbrs(site); 
+  % Compute dE using nbrs and check spin flip
+  dE = 2*S(site)*site_nbrs(S,site);
   spin_flip_prob = exp(-dE/kBT_J);
   if dE <= 0 || rand() <= spin_flip_prob
     S(site) = -S(site); 
@@ -105,8 +78,8 @@ function E = lattice_E(S, nbrs)
 end
 
 function M = lattice_M(S)
-  % Energy Hamiltonian of the whole matrix
-  M = sum(sum(S));
+  % Absolute magnetization
+  M = abs(mean(S, 'all'));
 end
 
 function nbrs = lattice_nbrs(S)
@@ -116,4 +89,15 @@ function nbrs = lattice_nbrs(S)
     + circshift(S, [0 -1]) ...
     + circshift (S,[1 0]) ...
     + circshift(S, [-1 0]);
+end
+
+function nbrs = site_nbrs(S, site)
+  % Get nbrs given site
+  N = size(S,1);
+  [i,j] = ind2sub([N N], site);
+  n = mod(i-2, N)+1;
+  s = mod(i, N)+1;
+  w  = mod(j-2,N) + 1;
+  e = mod(j, N)+1;
+  nbrs = S(n,j)+S(s,j)+S(i,e)+S(i,e);
 end
