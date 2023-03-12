0bad069d039937b9f9059561622877caac01c599
auan <august.forsman@outlook.com>
2023-03-12 02:02:56 +0100

    Toral implementation -> main file for P3

--- p3_auan.m
+++ p3_auan.m
@@ -1,113 +1,128 @@
+%% 2D ISING MODEL
+% Assumes square lattice
+
 clear
-% TeX for plots
-set(0, 'defaulttextinterpreter', 'latex')
-hAxes.TickLabelInterpreter = 'latex';
 
-% Lattice and sample size
-N = [8 16 32];
-N_samples = 100;
+% Params
+lattice = 2.^[3 4 5]; J = 1;
+M_therm = 1000; M = 1; MC = 1;
+T_max = 3.5; T_min = 1; T_steps = 100;
 
+% Start from max temp since thermalization
+% phase is cheaper
+kBTJ = linspace(T_max,T_min,T_steps)/J;
 
-% Length of phases
-therm_phase = 20;
+% Possible energy states of nearest nbrs
+E_pos = -4:2:4;
 
-% kBT/J is our unit (from hints)
-J = 1;
-unit_min = 0.5; 
-unit_max = 2; 
+% Alloc
+order = zeros(length(kBTJ), length(lattice));
+susc = zeros(length(kBTJ), length(lattice));
+% err_MAT = zeros(length(kBTJ), length(lattice));
+% mctau_MAT = zeros(length(kBTJ), length(lattice));
+% c_MAT= zeros(length(kBTJ), length(lattice));
 
-% Critical temp
-T_crit = 2/log(1+sqrt(2));
+for sz = 1:length(lattice)
+  % Lattice size for reuse
+  N = lattice(sz);
 
-% From high to low temp gives efficient therm phase
-kBT_J = T_crit*linspace(unit_max,unit_min,100)./J; 
+  % Simulation info
+  disp(['Lattice size N = ' num2str(N) ' on T = [' num2str(T_max) ',' num2str(T_min) ']']);
+  disp([num2str(M*N*N) ' sweeps over ' num2str(MC*N*N) ' MC steps']);
+  disp('********* Simulation started *********');
+  p1 = datetime(now,'ConvertFrom','datenum');
+  disp(p1);
 
-for sz = 1:length(N)
-  % Alloc 
-  S = lattice_init(N(sz));
-  M = zeros(length(N),length(kBT_J));
-  tic
-  % TODO: ising function as driver function
-  for i = 1:length(kBT_J)
-    M_MC = 0;
-    % Thermalization phase for each k_B*T/J
-    for j = 1:therm_phase
-      S = metropolis(S, kBT_J(i));
+  % Random spin matrix
+  S = lattice_init(N);
+
+  % Temperature loop
+  for i = 1:length(kBTJ)
+    % Populate Boltzmann-Gibbs factors
+    h = min(1,exp(-2*E_pos./kBTJ(i)));
+
+    % Thermalization phase
+    for ij = 1:M_therm*N*N
+        S = metropolis(S,N,h);
     end
 
-    % Sweep phase for each k_B*T/J
-    for j = 1:N_samples
-      for k = 1:N(sz)*N_samples
-        S = metropolis(S, kBT_J(i));
-        % Accumulate magnetization
-        M_MC = M_MC + lattice_M(S);
+    % Init vars
+    c = 0; rm = 0; rm2 = 0;
+    rm1 = abs(sum(sum(S)));
+
+    % MC phase
+    for im = 1:M*N*N
+      for ij = 1:MC*N*N
+        S = metropolis(S,N,h);
       end
-       % Compute average after every MC step
-      M(sz,i) = M_MC/(N(sz)*N_samples^2);
+
+      % Accumulate measurements
+      rm0 = abs(sum(sum(S)));
+      rm=rm+rm0;
+      rm2=rm2+rm0*rm0;
+      c=c+rm0*rm1;
+      rm1=rm0;
+
     end
+    % Averages
+    rm=rm/(M*N*N);
+    rm2=rm2/(M*N*N)-rm*rm;
+
+    % Correlation (unused)
+    % c=(c/M-rm*rm*N*N)/rm2*N*N;
+    % if (c~=1.0)
+    %   tau=c/(1-c);
+    % end
+
+    disp(['T = ' num2str(kBTJ(i)) ' [' num2str(i) '/' num2str(T_steps) ']'])
+    %err=sqrt(rm2*(2*tau+1)/M);
+    order(i,sz) = rm;
+    susc(i,sz) = N*rm2/kBTJ(i);
+    
+    % Unused at the moment
+    % err_MAT(i,sz) = err;
+    % mctau_MAT(i,sz) = MC*tau;
+    % c_MAT(i,sz) = c;
   end
-  toc
-
-figure(1);
-plot(kBT_J/T_crit,M(sz,:),'.')
-hold on
-grid on
-title(['Lattice sizes $N = {8,16,32}$'])
-% plot(kBT_J,M_mov_mean,'-', 'Linewidth',2)
-xlabel('$\frac{k_BT}{JT_c}$')
-% ylabel('<E>')
-ylabel('$|M|$')
-end
-T_crit_plot = linspace(0.5,1,1000)*T_crit;
-plot(T_crit_plot/T_crit, ...
-  real((1 - sinh(2*J*1./T_crit_plot).^-4).^(1/8)), ...
-  '-', 'LineWidth', 2);
-legend('N=8','N=16','N=32', 'T_c (critical)')
-hold off
-
-function S = metropolis(S, kBT_J)
-  % Assumes square spin lattice
-  N = size(S,1);
-  % Random lattice site
-  site = randi(N*N);
-  % Compute dE using nbrs and check spin flip
-  dE = 2*S(site)*site_nbrs(S,site);
-  spin_flip_prob = exp(-dE/kBT_J);
-  if dE <= 0 || rand() <= spin_flip_prob
-    S(site) = -S(site); 
-  end
+  p2 = datetime(now,'ConvertFrom','datenum');
+  disp('********* Simulation finished *********');
+  disp(p2-p1);
+  save('order.mat', 'order');
+  save('susc.mat', 'susc');
 end
 
-function spin = lattice_init(N)
-  % Assumes square spin lattice
-  spin = (-1).^randi(2,N,N);
-end
-
-function E = lattice_E(S, nbrs)
-  % Energy Hamiltonian of the whole matrix
-  E = -mean(S.*nbrs,'all');
-end
-
-function M = lattice_M(S)
-  % Absolute magnetization
-  M = abs(mean(S, 'all'));
-end
-
-function nbrs = lattice_nbrs(S)
-  % Circular shift to compute neighbours
-  % Similar to MPI_Cart_shift or np.roll
-  nbrs = circshift(S,[0 1]) ...
-    + circshift(S, [0 -1]) ...
-    + circshift (S,[1 0]) ...
-    + circshift(S, [-1 0]);
-end
+subplot(2,1,1)
+plot(kBTJ,(order./lattice.^2), '.-')
+legend
+title('Order')
+subplot(2,1,2)
+legend
+plot(kBTJ,susc./lattice.^2, '.-')
+title('Susceptibilty')
+legend
 
 function nbrs = site_nbrs(S, site)
   % Get nbrs given site
   N = size(S,1);
+  % Linear idx to matrix idx
   [i,j] = ind2sub([N N], site);
+  % Get nbrs state
   nbrs = S(mod(i-2, N)+1,j) ...
     + S(mod(i, N)+1,j) ...
     + S(i,mod(j, N)+1) ...
     + S(i,mod(j-2,N)+1);
+end
+
+function S = lattice_init(N)
+  S = (-1).^randi(2,N,N);
+end
+
+function S = metropolis(S,N,h)
+  % Random site in S
+  site = randi(N*N);
+  dE = S(site)*site_nbrs(S,site);
+  % Metropolis step
+  if dE <= 0 || rand() < h(((dE+4)/2+1))
+    S(site) = -S(site);
+  end
 end
