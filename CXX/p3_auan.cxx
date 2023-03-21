a8a8b2d8eb39a95bcf42cb16bed0d61d60488812
auan <august.forsman@outlook.com>
2023-03-21 11:34:47 +0100

    Added input args, data printed in columns

--- CXX/p3_auan.cxx
+++ CXX/p3_auan.cxx
@@ -1,7 +1,7 @@
 #include <NumCpp.hpp>
 #include <iostream>
 
-// Macros for readability
+// Macros
 #define SITE s[0],s[1]
 #define SZ N*N
 #define n1 (s[0]+1)%N, s[1]
@@ -12,8 +12,15 @@
 #define avgnorm(x) x/((double)M*N*N)
 #define norm(x) x/((double)N*N)
 
+// Columns
+#define TEMP 0
+#define ORDER 1
+#define CHI 2
+#define CB 3
+#define U 4
+
 // Global constants
-const int M = 11000; 
+const int M = 200000; 
 const int therm = 1000;
 const int MC = 1;
 
@@ -23,13 +30,20 @@ void metropolis(nc::NdArray<int>& S, int& N, nc::NdArray<double>& h);
 // Hamiltonian
 double H(nc::NdArray<int>& S, int& N);
 
-int main (int argc, char *argv[])
-{
-  std::ofstream out;
-  std::ifstream in;
-  out.open("Data/run.txt");
-  std::cout << "GO!" << std::endl;
+// Print data columns
+void tofile(nc::NdArray<double>& measurement, std::string file);
 
+int main (int argc, char *argv[]) {
+  if (argc < 3 || argc >= 4) {
+    std::cout << "USAGE: <lattice dim> <file>" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Input args
+  int N = atoi(argv[1]);
+  std::string file = argv[2];
+
+  // Temperature steps
   struct {
     const double max = 5;
     const double min = 0.1;
@@ -41,17 +55,15 @@ int main (int argc, char *argv[])
   // Measurements
   double m0 = 0;
   double m = 0;
+  double E0 = 0;
   double E = 0;
   double E2 = 0;
   double m2 = 0;
   double m4 = 0;
   double chi = 0;
-  double CB = 0;
-  double U = 0;
-  int E0 = 0;
-
-  // Lattice sizes
-  auto lattices = nc::NdArray<int>{8,16,32};
+  double cb = 0;
+  double u = 0;
+  int n_measures = 5;
 
   // To compute Boltzmann-Gibbs factors
   auto h = nc::NdArray<double>(1,5);
@@ -64,57 +76,63 @@ int main (int argc, char *argv[])
   auto E_pos = nc::linspace<double>(-4.0, 4.0, 5.0);
 
   // Order parameter lambda
-  auto order = [](auto S, auto N){return nc::abs(nc::sum<int>(S).item())/((double)SZ);};
+  auto order = [](auto S, auto N){
+    return nc::abs(nc::sum<int>(S).item())/((double)SZ);
+  };
 
-  // Temperature loop
-  for (auto& N : lattices) {
-    // Init random matrix
-    auto S = nc::random::choice<int>({-1,1},SZ).reshape(N,N);
-    for (auto& T : kBTJ) {
-      // Populate G-B factors
-      h = nc::minimum(one, nc::exp(-2.0*E_pos/T));
+  // Init random matrix
+  auto S = nc::random::choice<int>({-1,1},SZ).reshape(N,N);
 
-      // Thermalization
-      for (size_t i = 0; i < therm*SZ; i++) {
-          metropolis(S, N, h);
-      }
+  // Measure arrs
+  auto measurement = nc::NdArray<double>(kBTJ.shape().cols,n_measures);
 
-      // Reset measurements
-      m = 0; m2 = 0; chi = 0; E = 0; E2 = 0;
+  for (size_t T = 0; T < kBTJ.shape().cols; T++) {
+    // Populate G-B factors
+    h = nc::minimum(one, nc::exp(-2.0*E_pos/kBTJ[T]));
 
-      // Simulation and measurements
-      for (size_t i = 0; i < M; i++) {
-        for (size_t j = 0; j < MC*SZ; j++) {
-          metropolis(S, N, h);
-        }
-
-        // Order param
-        m0 = order(S,N);
-        // Energy
-        // E0 = H(S,N);
-
-        // Accumulate
-        m += m0;
-        m2 += m0*m0;
-        m4 += m0*m0*m0*m0;
-        // E += E0;
-        // E2 += E0*E0;
-      }
-      // Averages over M samples
-      m = m/M;
-      m2 = m2/M;
-      m4 = m4/M;
-      // E = E/M;
-      E2 = E2/M;
-
-      chi = SZ*(m2-m*m)/(T); // Susceptibilty
-      // CB = (E2-E*E)/(T*T); // Specific heat 
-      U = 1-m4/(3*(m2*m2)); // Binder's parameter
-
-      // Write to file
-      out << m << ' ' << chi << ' ' << CB << ' ' << U << ' ' << T << "\n";
+    // Thermalization
+    for (size_t i = 0; i < therm*SZ; i++) {
+      metropolis(S, N, h);
     }
+
+    // Reset measurements
+    m = 0; m2 = 0; m4 = 0; E = 0; E2 = 0;
+
+    // Simulation and measurements
+    for (size_t i = 0; i < M; i++) {
+      for (size_t j = 0; j < MC*SZ; j++) {
+        metropolis(S, N, h);
+      }
+
+      // Order param
+      m0 = order(S,N);
+      // Energy
+      E0 = H(S,N);
+
+      // Accumulate
+      m += m0;
+      m2 += m0*m0;
+      m4 += m0*m0*m0*m0;
+      E += E0;
+      E2 += E0*E0;
+    }
+    // Average over M samples
+    m = m/M;
+    m2 = m2/M;
+    m4 = m4/M;
+    E = E/M;
+    E2 = E2/M;
+
+    // Add to measurements
+    measurement(T,TEMP) = kBTJ[T];
+    measurement(T,ORDER) = m;
+    measurement(T,CHI) = SZ*(m2-m*m)/(kBTJ[T]);
+    measurement(T,CB) = (E2-E*E)/(kBTJ[T]*kBTJ[T]);
+    measurement(T,U) = 1-m4/(3*(m2*m2));
+
   }
+  // Print columns
+  tofile(measurement, file);
   return 0;
 }
 
@@ -133,4 +151,18 @@ double H(nc::NdArray<int>& S, int& N) {
     + nc::roll(S, 1, nc::Axis::ROW)
     + nc::roll(S, -1, nc::Axis::ROW);
   return -nc::abs(nc::sum(nc::matmul(S,nbrs))).item()/((double)SZ);
+}
+
+// Write columns to file
+void tofile(nc::NdArray<double>& measurement, std::string file) {
+  std::ofstream out;
+  out.open("Data/"+file);
+  // Header
+  out << "temp " << "order " << "chi " << "cb " << "u" << std::endl;
+  for (size_t i = 0; i < measurement.shape().rows; i++) {
+    for (size_t j = 0; j < measurement.shape().cols; j++) {
+      out << measurement(i,j) << ' ';
+    }
+    out << std::endl;
+  }
 }
